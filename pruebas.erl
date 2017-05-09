26990bd4ae9e7d5c74cff489b24f6cc8c066f9e3
rubentl <polipo86@gmail.com>
2017-05-09 14:40:29 +0200

    terminada la web de limpieza y más pruebas de erlang

--- pruebas.erl
+++ pruebas.erl
@@ -2,6 +2,8 @@
 -author("Rubén Toca").
 -vsn("0.0.1").
 
+-include_lib("c:/Archivos de programa/erl8.3/lib/stdlib-3.3/include/qlc.hrl").
+
 -export([main/0]).
 
 -record(libro,{
@@ -15,6 +17,16 @@
             ubicacion=[]
          }).
 
+query(Query) ->
+    F = fun() ->
+                qlc:e(Query)
+        end,
+    {atomic, Valor} = mnesia:transaction(F),
+    Valor.
+
+info(Data) ->
+    error_logger:info_report(Data).
+
 crear_tablas() ->
     mnesia:create_table(
         libro,
@@ -22,6 +34,37 @@ crear_tablas() ->
              {disc_copies, [node()]},
              {type, bag}]).
 
+libro_add(Record) ->
+    F = fun() ->
+                mnesia:write(Record)
+        end,
+    {atomic, ok} = mnesia:transaction(F),
+    ok.
+
+libro_read(Query) ->
+    Fun = fun() ->
+                  mnesia:match_object(Query)
+          end,
+    {atomic, Valor} = mnesia:transaction(Fun),
+    Valor.
+
 main() ->
     application:start(mnesia),
-    io_lib:format("Crear tabla: ~s~n", [crear_tablas()]).
+    
+    Record = #libro{
+                titulo = <<"Escrito en el agua">>,
+                autor = [<<"Paula Hawkins">>],
+                editorial = <<"Planeta">>,
+                ano = 2017,
+                isbn = 9788408172178,
+                descripcion = <<"Pocos días antes de morir, Nel Abbott estuvo llamando a su hermana, pero Jules no cogió el teléfono, ignoró sus súplicas de ayuda. Ahora Nel está muerta. Dicen que saltó al río. Y Jules se ve arrastrada al pequeño pueblo de los veranos de su infancia, un lugar del que creía haber escapado, para cuidar de la adolescente que su hermana deja atrás. Pero Jules tiene miedo. Mucho miedo. Miedo al agua, miedo de sus recuerdos enterrados largo tiempo atrás, y miedo,
+                                sobre todo, de su certeza de que Nel nunca habría saltado?"/utf8>>,
+                encuadernacion = <<"Tapa blanda">>,
+                ubicacion = [<<"La casa del libro">>]
+               },
+    libro_add(Record),
+    Q = qlc:q([L || L <- mnesia:table(libro)], cache),
+    info(qlc:info(Q)),
+    Result = query(Q),
+    info(Result),
+    application:stop(mnesia).
