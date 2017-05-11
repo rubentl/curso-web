b29ed7287f4b795e7373c8a771303d94b919a048
rubentl <polipo86@gmail.com>
2017-05-11 14:45:56 +0200

    más erlang y completar ejercicio 23

--- pruebas.erl
+++ pruebas.erl
@@ -6,16 +6,53 @@
 
 -export([main/0]).
 
--record(libro,{
+-record(noticia,{
             titulo,
-            autor=[],
-            editorial,
-            ano,
-            isbn,
-            descripcion,
-            encuadernacion,
-            ubicacion=[]
+            texto,
+            fecha = {}
          }).
+
+-record(evento,{
+            titulo,
+            fecha,
+            lugar,
+            responsable,
+            enlace
+         }).
+
+-record(contacto,{
+            nombre,
+            direccion,
+            telefono,
+            horario
+         }).
+
+tablas() -> [noticia, evento, contacto].
+datos() -> [
+            #noticia{titulo = <<"Campamento"/utf8>>,
+                     texto = <<"Comienzan las subscripciones para el campamento de verano."/utf8>>,
+                     fecha = calendar:local_time()},
+            #noticia{titulo = <<"Semana santa"/utf8>>,
+                     texto = <<"Comienza la Semana Santa"/utf8>>,
+                     fecha = calendar:local_time()},
+            #evento{titulo = <<"Fiesta de Mayores"/utf8>>,
+                    fecha = {{2017,11,10},{18,30,00}},
+                    lugar = <<"Locales parroquiales"/utf8>>,
+                    responsable = <<"Pepe"/utf8>>,
+                    _ = '_'},
+            #evento{titulo = <<"Viaje de Santa Ana"/utf8>>,
+                    fecha = {{2017,08,01},{07,00,00}},
+                    lugar = <<"frente a la parroquia"/utf8>>,
+                    _ = '_'},
+            #contacto{nombre = <<"José García Albatros"/utf8>>, 
+                      direccion = <<"C/ Los ciruelos nº 34, Santander"/utf8>>,
+                      telefono = <<"555-555-555"/utf8>>,
+                      horario = <<"Lunes a Viernes: 15:00 a 19:00"/utf8>>},
+            #contacto{nombre = <<"Amalia Sánchez Camino"/utf8>>, 
+                      direccion = <<"C/ Héroes de Mayo nº 1, Herrera de Camargo"/utf8>>, 
+                      telefono = <<"000-444-555"/utf8>>,
+                      horario = <<"">>}
+           ].
 
 query(Query) ->
     F = fun() ->
@@ -27,44 +64,30 @@ query(Query) ->
 info(Data) ->
     error_logger:info_report(Data).
 
-crear_tablas() ->
+crear_tabla(Tabla) ->
     mnesia:create_table(
-        libro,
-            [{attributes, record_info(fields, libro)},
-             {disc_copies, [node()]},
-             {type, bag}]).
+            Tabla,
+            [{attributes, record_info(fields, Tabla)},
+             {disc_copies, [node()]}
+            ]).
 
-libro_add(Record) ->
+write(Record) ->
     F = fun() ->
                 mnesia:write(Record)
         end,
     {atomic, ok} = mnesia:transaction(F),
     ok.
 
-libro_read(Query) ->
-    Fun = fun() ->
-                  mnesia:match_object(Query)
-          end,
-    {atomic, Valor} = mnesia:transaction(Fun),
-    Valor.
+preparar_data(Tablas, Datos) ->
+    [crear_tabla(Tabla) || Tabla <- Tablas],
+    [write(Record) || Record <- Datos].
 
 main() ->
     application:start(mnesia),
-    
-    Record = #libro{
-                titulo = <<"Escrito en el agua">>,
-                autor = [<<"Paula Hawkins">>],
-                editorial = <<"Planeta">>,
-                ano = 2017,
-                isbn = 9788408172178,
-                descripcion = <<"Pocos días antes de morir, Nel Abbott estuvo llamando a su hermana, pero Jules no cogió el teléfono, ignoró sus súplicas de ayuda. Ahora Nel está muerta. Dicen que saltó al río. Y Jules se ve arrastrada al pequeño pueblo de los veranos de su infancia, un lugar del que creía haber escapado, para cuidar de la adolescente que su hermana deja atrás. Pero Jules tiene miedo. Mucho miedo. Miedo al agua, miedo de sus recuerdos enterrados largo tiempo atrás, y miedo,
-                                sobre todo, de su certeza de que Nel nunca habría saltado?"/utf8>>,
-                encuadernacion = <<"Tapa blanda">>,
-                ubicacion = [<<"La casa del libro">>]
-               },
-    libro_add(Record),
+    preparar_data(tablas(), datos()),
     Q = qlc:q([L || L <- mnesia:table(libro)], cache),
     info(qlc:info(Q)),
     Result = query(Q),
     info(Result),
+
     application:stop(mnesia).
